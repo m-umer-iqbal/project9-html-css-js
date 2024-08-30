0e9b81132abbcbe00c6ed932d18cf587b31eae2f
Umer Iqbal <142382304+m-umer-iqbal@users.noreply.github.com>
2024-08-30 05:22:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        let buttonClicked = this.innerHTML;
+        checkKey(buttonClicked);
+        buttonAnimation(buttonClicked);
+    });
+}
+document.addEventListener("keydown", function(event){
+    checkKey(event.key);
+    buttonAnimation(event.key);
+});
+function checkKey(key){
+    switch(key){
+        case "a":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "d":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "f":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            alert(buttonClicked);
+            break;
+    }
+}
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector(`.${currentKey}`);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
